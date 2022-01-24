7d65e290f4a53d36b2681a067b747d231519d7aa
Macarena Acosta <macarenaa988@gmail.com>
2022-01-24 19:28:22 -0300

    class that contains the endpoints of the api

---
+++ main/test/java/consts/ProductsEndpoints.java
@@ -0,0 +1,6 @@
+package consts;
+
+public class ProductsEndpoints {
+    public static final String ID_PRODUCT_URL = "/products/{id}";
+    public static final String ALL_PRODUCTS_URL = "/products";
+}
